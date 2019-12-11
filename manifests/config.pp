0452583ffc78246268e5c00fba84005eb9782ccb
Marco Centurion <mcenturion@protonmail.com>
2019-12-11 16:08:26 -0300

    Fix syntax error

--- manifests/config.pp
+++ manifests/config.pp
@@ -194,8 +194,8 @@ class rundeck::config {
   } else {
     file_line {"${properties_dir}/profile":
       ensure => present,
-      line => "RDECK_HTTPS_PORT=\${RDECK_HTTPS_PORT:-${ssl_port}}"
-      match => 'RDECK_HTTPS_PORT=${RDECK_HTTPS_PORT:-[0-9]+}'
+      line   => "RDECK_HTTPS_PORT=\${RDECK_HTTPS_PORT:-${ssl_port}}",
+      match  => 'RDECK_HTTPS_PORT=${RDECK_HTTPS_PORT:-[0-9]+}',
     }
   }
 
