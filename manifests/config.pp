fe2e41cb0bfbf4b895a0ae452e71fa2dde4c3123
Marco Centurion <mcenturion@protonmail.com>
2019-12-11 16:16:29 -0300

    Change match

--- manifests/config.pp
+++ manifests/config.pp
@@ -196,7 +196,7 @@ class rundeck::config {
       ensure => present,
       path   => "${properties_dir}/profile",
       line   => "RDECK_HTTPS_PORT=\${RDECK_HTTPS_PORT:-${ssl_port}}",
-      match  => 'RDECK_HTTPS_PORT=${RDECK_HTTPS_PORT:-[0-9]+}',
+      match  => '^RDECK_HTTPS_PORT=',
     }
   }
 
